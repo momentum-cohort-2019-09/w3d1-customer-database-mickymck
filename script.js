f04e4a6de88d1f0c3b993c267f18c112b58ff9f3
mickymck <mickymckeon@gmail.com>
2019-09-23 20:11:48 -0400

    Finished homework assignment

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const capitalize = string => string[0].toUpperCase() + string.slice(1)
+// const capitalize = string => string[0].toUpperCase() + string.slice(1)
 
 // similar to this, but use indexOf to find the location of the first character that is not a number (NaN)
 
 function customerToHtml(customer) {
     return `
     <div class="profile">
-        <div class=" profileImg">
-            <img src="${customer.picture.large}">
+        <div class=" imgDiv">
+            <img src="${customer.picture.large}" class="profileImg alt="${(customer.name.first)} ${(customer.name.last)}">
         </div>
-        <div class="profileName">${capitalize(customer.name.first)} ${capitalize(customer.name.last)}</div>
+        <div class="profileName">${(customer.name.first)} ${(customer.name.last)}</div>
         <div class="profileEmail">${customer.email}</div>
-        <div class="profileAdd1">${capitalize(customer.location.street)}</div>
+        <div class="profileAdd1">${(customer.location.street)}</div>
         <div class="profileAdd2">${customer.location.city}, ${customer.location.state}, ${customer.location.postcode}</div>
-        <div class="profileDateOfBirth">DOB: ${customer.dob}</div>
-        <div class="profileSignUpDate">${customer.registered}</div>
+        <div class="profileDateOfBirth">DOB: ${moment(customer.dob).format('ll')}</div>
+        <div class="profileSignUpDate">Customer since: ${moment(customer.registered).format('ll')}</div>
     </div>
     `
 }
