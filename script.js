99a95dc18866b4a73d7c248a0a06bbf00b469435
mickymck <mickymckeon@gmail.com>
2019-09-24 10:12:34 -0400

    fixed the responsiveness and abbreviated state names

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // similar to this, but use indexOf to find the location of the first character that is not a number (NaN)
 
+
 function customerToHtml(customer) {
     return `
     <div class="profile">
@@ -11,11 +12,12 @@ function customerToHtml(customer) {
         <div class="profileName">${(customer.name.first)} ${(customer.name.last)}</div>
         <div class="profileEmail">${customer.email}</div>
         <div class="profileAdd1">${(customer.location.street)}</div>
-        <div class="profileAdd2">${customer.location.city}, ${customer.location.state}, ${customer.location.postcode}</div>
+        <div class="profileAdd2">${customer.location.city}, ${nameToAbbr(customer.location.state)} ${customer.location.postcode}</div>
         <div class="profileDateOfBirth">DOB: ${moment(customer.dob).format('ll')}</div>
         <div class="profileSignUpDate">Customer since: ${moment(customer.registered).format('ll')}</div>
     </div>
     `
+
 }
 
 document.querySelector("#database").innerHTML = customers.map(customerToHtml).join("\n")
